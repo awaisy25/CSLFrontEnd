8638c1a61219110e5f9c8b24a70537235b9057f8
Awais Hussain <awais@Awaiss-MacBook-Pro.local>
2021-08-22 19:25:25 -0400

    Basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react';
 import './App.css';
-//some comment
+
+const formChangeData = (state, event) => {
+  //console.log(`Key: ${event.name}, Value: ${event.value}`)
+  return {
+    //JavaScript spread .. gets rest of the data
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [formData, dispatch] = useReducer(formChangeData, {});
+  const [isSubmit, setSubmitted] = useState(false);
+
+  const handleSubmit = event => {
+   event.preventDefault();
+   setSubmitted(true);
+   alert('You have submitted the form.')
+ }
+
+ const handleChange = event => {
+   dispatch({
+     name: event.target.name,
+     value: event.target.value
+   })
+ }
+
+  return(
+    <div className="wrapper">
+      <h1>Calculate Student Loans</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      {isSubmit && 
+        <div> 
+          <h2>Results</h2>
+          <ul>
+            {
+              // eslint-disable-next-line array-callback-return
+              Object.entries(formData).map(([name, value]) => (
+                <li key={name}><strong>{name}</strong>: {value.toString()}</li>
+              ))
+            }
+          </ul>
+        </div>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
